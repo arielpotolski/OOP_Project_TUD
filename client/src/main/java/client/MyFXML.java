68fda3cc689a5e03b1035e26c73f46d13394dd2c
Thomas Voss <t.a.voss@student.tudelft.nl>
2022-02-25 17:07:55 +0100

    Fix checkstyle config and remove annoying options

--- client/src/main/java/client/MyFXML.java
+++ client/src/main/java/client/MyFXML.java
@@ -21,7 +21,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 import com.google.inject.Injector;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.util.Builder;
@@ -30,45 +29,45 @@ import javafx.util.Callback;
 import javafx.util.Pair;
 
 public class MyFXML {
+	private Injector injector;
 
-    private Injector injector;
+	public MyFXML(Injector injector) {
+		this.injector = injector;
+	}
 
-    public MyFXML(Injector injector) {
-        this.injector = injector;
-    }
+	public <T> Pair<T, Parent> load(Class<T> c, String... parts) {
+		try {
+			var loader = new FXMLLoader(getLocation(parts), null, null, new MyFactory(),
+										StandardCharsets.UTF_8);
+			Parent parent = loader.load();
+			T ctrl = loader.getController();
+			return new Pair<>(ctrl, parent);
+		} catch (IOException err) {
+			throw new RuntimeException(err);
+		}
+	}
 
-    public <T> Pair<T, Parent> load(Class<T> c, String... parts) {
-        try {
-            var loader = new FXMLLoader(getLocation(parts), null, null, new MyFactory(), StandardCharsets.UTF_8);
-            Parent parent = loader.load();
-            T ctrl = loader.getController();
-            return new Pair<>(ctrl, parent);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private URL getLocation(String... parts) {
+		var path = Path.of("", parts).toString();
+		return MyFXML.class.getClassLoader().getResource(path);
+	}
 
-    private URL getLocation(String... parts) {
-        var path = Path.of("", parts).toString();
-        return MyFXML.class.getClassLoader().getResource(path);
-    }
+	private class MyFactory implements BuilderFactory, Callback<Class<?>, Object> {
 
-    private class MyFactory implements BuilderFactory, Callback<Class<?>, Object> {
+		@Override
+		@SuppressWarnings("rawtypes")
+		public Builder<?> getBuilder(Class<?> type) {
+			return new Builder() {
+				@Override
+				public Object build() {
+					return injector.getInstance(type);
+				}
+			};
+		}
 
-        @Override
-        @SuppressWarnings("rawtypes")
-        public Builder<?> getBuilder(Class<?> type) {
-            return new Builder() {
-                @Override
-                public Object build() {
-                    return injector.getInstance(type);
-                }
-            };
-        }
-
-        @Override
-        public Object call(Class<?> type) {
-            return injector.getInstance(type);
-        }
-    }
+		@Override
+		public Object call(Class<?> type) {
+			return injector.getInstance(type);
+		}
+	}
 }
