cc53b6e381c929821b6b733c239fb22253788395
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-02-25 19:40:51 +0100

    Add GetByID endpoint to the project.

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import commons.Activity;
 import server.database.ActivityRepository;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,14 @@ public class ActivityController {
 	@GetMapping(path = {"", "/"})
 	public List<Activity> getAll() {
 		return  repository.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Activity> getById(@PathVariable("id") String id) {
+		if ("".equals(id) || !repository.existsById(id)) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok(repository.getById(id));
 	}
 
 }
