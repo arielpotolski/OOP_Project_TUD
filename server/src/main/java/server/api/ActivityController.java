769ec8e30778ce32da0d050b78152bc9a190a6e2
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2022-02-25 22:45:50 +0100

    Add POST endpoint for creating activities

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import commons.Activity;
 import server.database.ActivityRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +25,28 @@ public class ActivityController {
 		this.repository = repository;
 	}
 
+	private static boolean isNullOrEmpty(String s) {
+		return s == null || s.isEmpty();
+	}
+
 	@GetMapping(path = {"", "/"})
 	public List<Activity> getAll() {
 		return  repository.findAll();
 	}
 
+	@PostMapping(path = {"", "/"},
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Activity> create(@RequestBody Activity newActivity) {
+		if (
+			newActivity == null ||
+			isNullOrEmpty(newActivity.getId()) ||
+			isNullOrEmpty(newActivity.getTitle()) ||
+			isNullOrEmpty(newActivity.getSource())
+		) {
+			return ResponseEntity.badRequest().build();
+		}
+		Activity activity = this.repository.save(newActivity);
+		return new ResponseEntity<>(activity, HttpStatus.CREATED);
+	}
 }
