d2629a1502ea0e1cd9277f076f294f1c27e1068a
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-02-25 17:40:55 +0100

    Fixed checkstyle pipeline Try 2

--- commons/src/test/java/commons/ActivityTest.java
+++ commons/src/test/java/commons/ActivityTest.java
@@ -3,80 +3,82 @@ package commons;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ActivityTest {
 
-    private Activity activity1;
-    private Activity activity2;
-    private Activity activity3;
+	private Activity activity1;
+	private Activity activity2;
+	private Activity activity3;
 
-    @BeforeEach
-    void setUp(){
-        activity1 = new Activity("123", "title", 230, "some site");
-        activity2 = new Activity("123", "title", 230, "some site");
-        activity3 = new Activity("1233", "title", 230, "some site");
-    }
+	@BeforeEach
+	void setUp(){
+		activity1 = new Activity("123", "title", 230, "some site");
+		activity2 = new Activity("123", "title", 230, "some site");
+		activity3 = new Activity("1233", "title", 230, "some site");
+	}
 
-    @Test
-    public void checkConstructor() {
-        assertNotNull(activity1);
-    }
-
-
-    @Test
-    void getId() {
-        assertEquals("123", activity1.getId());
-    }
-
-    @Test
-    void getTitle() {
-        assertEquals("title", activity1.getTitle());
-    }
-
-    @Test
-    void getConsumptionInWh() {
-        assertEquals(230, activity1.getConsumptionInWh());
-    }
-
-    @Test
-    void getSource() {
-        assertEquals("some site", activity1.getSource());
-    }
+	@Test
+	public void checkConstructor() {
+		assertNotNull(activity1);
+	}
 
 
-    @Test
-    void setId() {
-        activity1.setId("234");
-        assertEquals("234", activity1.getId());
-    }
+	@Test
+	void getId() {
+		assertEquals("123", activity1.getId());
+	}
 
-    @Test
-    void setTitle() {
-        activity1.setTitle("title2");
-        assertEquals("title2", activity1.getTitle());
-    }
+	@Test
+	void getTitle() {
+		assertEquals("title", activity1.getTitle());
+	}
 
-    @Test
-    void setConsumptionInWh() {
-        activity1.setConsumptionInWh(420);
-        assertEquals(420, activity1.getConsumptionInWh());
-    }
+	@Test
+	void getConsumptionInWh() {
+		assertEquals(230, activity1.getConsumptionInWh());
+	}
 
-    @Test
-    void setSource() {
-        activity1.setSource("some other site");
-        assertEquals("some other site", activity1.getSource());
-    }
+	@Test
+	void getSource() {
+		assertEquals("some site", activity1.getSource());
+	}
 
-    @Test
-    void testEqualsMethod() {
-        assertEquals(activity1, activity2);
-        assertNotEquals(activity1, activity3);
-    }
 
-    @Test
-    void testHashMethod() {
-        assertEquals(activity1.hashCode(), activity2.hashCode());
-    }
+	@Test
+	void setId() {
+		activity1.setId("234");
+		assertEquals("234", activity1.getId());
+	}
+
+	@Test
+	void setTitle() {
+		activity1.setTitle("title2");
+		assertEquals("title2", activity1.getTitle());
+	}
+
+	@Test
+	void setConsumptionInWh() {
+		activity1.setConsumptionInWh(420);
+		assertEquals(420, activity1.getConsumptionInWh());
+	}
+
+	@Test
+	void setSource() {
+		activity1.setSource("some other site");
+		assertEquals("some other site", activity1.getSource());
+	}
+
+	@Test
+	void testEqualsMethod() {
+		assertEquals(activity1, activity2);
+		assertNotEquals(activity1, activity3);
+	}
+
+	@Test
+	void testHashMethod() {
+		assertEquals(activity1.hashCode(), activity2.hashCode());
+	}
 }
