cf934cdabd659b0eae48ef89285a6690f2277959
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-02-27 18:07:22 +0100

    Add Scene for Single Player for Questions

---
+++ client/src/main/java/client/scenes/QuestionScreenSinglePlayerCtrl.java
@@ -0,0 +1,36 @@
+package client.scenes;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+
+public class QuestionScreenSinglePlayerCtrl {
+
+	@FXML
+	private javafx.scene.control.Button answerButton1;
+
+	@FXML
+	private Button answerButton2;
+
+	@FXML
+	private Button answerButton3;
+
+	@FXML
+	private Button answerButton4;
+
+	@FXML
+	private Label labelQuestion;
+
+	@FXML
+	private Label labelTimer;
+
+	@FXML
+	private ImageView imageViewQuestion;
+
+	@FXML
+	private Circle circleTimer;
+}
