c0eff47650bff5f42d47b6b39abd0f9793124f3d
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-03-02 12:59:04 +0100

    Fix whitespaces so that they do not look sus

--- client/src/main/java/client/scenes/QuestionScreenSinglePlayerCtrl.java
+++ client/src/main/java/client/scenes/QuestionScreenSinglePlayerCtrl.java
@@ -1,12 +1,10 @@
 package client.scenes;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
-
 
 public class QuestionScreenSinglePlayerCtrl {
 
