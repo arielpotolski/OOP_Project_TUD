73bcf8a3327a57cce56a3730f38efae5504a05ad
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-03-06 18:24:04 +0100

    Add intermediate Leaderboard scene and scene controller.

---
+++ client/src/main/java/client/scenes/IntLeaderboardCtrl.java
@@ -0,0 +1,12 @@
+package client.scenes;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+
+public class IntLeaderboardCtrl {
+
+    @FXML
+    private Rectangle timeUntilNextQuestion;
+
+}
