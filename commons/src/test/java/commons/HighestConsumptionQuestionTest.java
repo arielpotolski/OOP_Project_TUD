bc62cb81712e486de784201cda4912b290dd65ec
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-03-07 19:51:05 +0100

    Fix pipeline (hopefully).

--- commons/src/test/java/commons/HighestConsumptionQuestionTest.java
+++ commons/src/test/java/commons/HighestConsumptionQuestionTest.java
@@ -2,9 +2,10 @@ package commons;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HighestConsumptionQuestionTest{
 
