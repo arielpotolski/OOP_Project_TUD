c79d847cf29e3a6da0f6a425ed66c38705c038d2
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-03-08 17:36:25 +0100

    Fix pipeline (extra empty line).

--- commons/src/test/java/commons/EstimateQuestionTest.java
+++ commons/src/test/java/commons/EstimateQuestionTest.java
@@ -2,7 +2,6 @@ package commons;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
