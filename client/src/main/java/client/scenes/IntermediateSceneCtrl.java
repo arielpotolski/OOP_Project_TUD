b10fe7683e67b5e067d1fcc846f7a1f3dd233a74
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-03-13 19:12:23 +0100

    Fix controller for the progressBar.

--- client/src/main/java/client/scenes/IntermediateSceneCtrl.java
+++ client/src/main/java/client/scenes/IntermediateSceneCtrl.java
@@ -2,12 +2,13 @@ package client.scenes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.shape.Rectangle;
 
 public class IntermediateSceneCtrl {
 
 	@FXML
-	private Rectangle timeUntilNextQuestion;
+	private ProgressBar timeUntilNextQuestion;
 
 	@FXML
 	private Label playerPoints;
