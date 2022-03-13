9f630f9142a209687e29fa7b1b5ba137ba1607d3
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-03-13 19:15:09 +0100

    Fix pipeline (checkstyle failure).

--- client/src/main/java/client/scenes/IntermediateSceneCtrl.java
+++ client/src/main/java/client/scenes/IntermediateSceneCtrl.java
@@ -3,7 +3,6 @@ package client.scenes;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.shape.Rectangle;
 
 public class IntermediateSceneCtrl {
 
