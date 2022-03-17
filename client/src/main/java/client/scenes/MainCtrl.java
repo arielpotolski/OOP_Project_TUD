d936b43f6dcc0fd380d42aeca1de96998d1d207a
Viet Luong Hoang <LuongHoangViet@student.tudelft.nl>
2022-03-17 22:16:44 +0100

    Change the Progess Bar

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -150,9 +150,7 @@ public class MainCtrl {
 	 * This method shows up the question screen in single player mode.
 	 */
 	public void showQuestionScreenSinglePlayer() {
-		// Assign the player variable that got from
-		// SinglePlayerPreGame to player variable in MainCtrl
-		player = singlePlayerPreGameCtrl.getPlayer();
+
 
 		// If the size of question set equals to zero, this method change to final screen.
 		if (questions.size() == 0) {
@@ -160,8 +158,12 @@ public class MainCtrl {
 			return;
 		}
 
+		// Assign the player variable that got from
+		// SinglePlayerPreGame to player variable in MainCtrl
+		player = singlePlayerPreGameCtrl.getPlayer();
+
 		// This timeline will execute on another thread - run the count-down timer.
-		timeLine = new Timeline(new KeyFrame(Duration.seconds(1), _e -> {
+		timeLine = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
 			questionScreenSinglePlayerCtrl.decreaseProgress();
 		}));
 		timeLine.setCycleCount(10);
