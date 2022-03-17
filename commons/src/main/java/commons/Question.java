0b0f65a1c07f21e2e0327cbcf430e2ae989c5796
Viet Luong Hoang <LuongHoangViet@student.tudelft.nl>
2022-03-17 11:40:57 +0100

    Stage all changes

--- commons/src/main/java/commons/Question.java
+++ commons/src/main/java/commons/Question.java
@@ -10,7 +10,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonSubTypes({
 		@JsonSubTypes.Type(value = EstimateQuestion.class, name = "EstimateQuestion"),
 		@JsonSubTypes.Type(value = MCQuestion.class, name = "MCQuestion"),
-		@JsonSubTypes.Type(value = HighestConsumptionQuestion.class, name = "HighestConsumptionQuestion"),
+		@JsonSubTypes.Type(value = HighestConsumptionQuestion.class,
+				name = "HighestConsumptionQuestion"),
 		@JsonSubTypes.Type(value = InsteadOfQuestion.class, name = "InsteadOfQuestion")
 })
 public abstract class Question {
