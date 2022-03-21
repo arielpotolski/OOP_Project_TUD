505da6fb7ae42c17efa1423d556f4ec1bbca317b
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2022-03-21 17:35:56 +0100

    Join lobby from multiplayer pre-game scene

--- client/src/main/java/client/scenes/MultiplayerPreGameCtrl.java
+++ client/src/main/java/client/scenes/MultiplayerPreGameCtrl.java
@@ -1,11 +1,15 @@
 package client.scenes;
 
+import java.util.Optional;
+
 import client.utils.ServerUtils;
+import commons.LobbyResponse;
 
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apache.commons.lang3.NotImplementedException;
 
 public class MultiplayerPreGameCtrl {
 	private final ServerUtils server;
@@ -30,5 +34,20 @@ public class MultiplayerPreGameCtrl {
 	public MultiplayerPreGameCtrl(ServerUtils server, MainCtrl mainCtrl) {
 		this.mainCtrl = mainCtrl;
 		this.server = server;
+	}
+
+	public void joinLobby() {
+		String url = this.serverURL.getText();
+		String name = this.nickname.getText();
+
+		ServerUtils serverUtils = new ServerUtils(url);
+		Optional<LobbyResponse> maybeResponse = serverUtils.connectToLobby(name);
+		if (maybeResponse.isPresent()) {
+			this.mainCtrl.setServer(serverUtils);
+			this.mainCtrl.showWaitingScreen(maybeResponse.get());
+		} else {
+			// TODO tell user that the name is not available or that something went wrong
+			throw new NotImplementedException();
+		}
 	}
 }
