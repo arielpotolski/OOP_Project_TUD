2940656d4d8e2291841f71b151fd28cdf0b389c0
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2022-03-21 17:44:38 +0100

    Fix existing tests and add a new one

--- server/src/test/java/server/LobbyControllerTest.java
+++ server/src/test/java/server/LobbyControllerTest.java
@@ -1,12 +1,18 @@
 package server;
 
+import commons.Connection;
 import commons.LobbyResponse;
 
+import commons.messages.JoinMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LobbyControllerTest {
 	@Test
@@ -18,7 +24,7 @@ public class LobbyControllerTest {
 		assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
 		// Check that the number of players is 1.
 		// The ID is an implementation detail.
-		assertEquals(1, response.getBody().playersInLobby());
+		assertEquals(1, response.getBody().playersInLobby().size());
 	}
 
 	@Test
@@ -44,7 +50,7 @@ public class LobbyControllerTest {
 		assertNotEquals(charlie.playerID(), alice.playerID());
 
 		// Charlie was last to connect, so they have the latest information.
-		assertEquals(3, charlie.playersInLobby());
+		assertEquals(3, charlie.playersInLobby().size());
 	}
 
 	@Test
@@ -68,7 +74,7 @@ public class LobbyControllerTest {
 		// Check that player ID stays the same.
 		assertEquals(alice.playerID(), response.getBody().playerID());
 		// Check that the number of players stays one.
-		assertEquals(1, response.getBody().playersInLobby());
+		assertEquals(1, response.getBody().playersInLobby().size());
 	}
 
 	@Test
@@ -107,7 +113,7 @@ public class LobbyControllerTest {
 		LobbyResponse newAlice = lobby.registerPlayer("Alice").getBody();
 		assertNotEquals(alice.playerID(), newAlice.playerID());
 		// Check that the number of players is still one.
-		assertEquals(1, alice.playersInLobby());
+		assertEquals(1, alice.playersInLobby().size());
 	}
 
 	@Test
@@ -117,15 +123,43 @@ public class LobbyControllerTest {
 		assertEquals(HttpStatus.ACCEPTED, lobby.registerPlayer("Alice").getStatusCode());
 		LobbyResponse bob = lobby.registerPlayer("Bob").getBody();
 		// There are two players in the lobby.
-		assertEquals(2, bob.playersInLobby());
+		assertEquals(2, bob.playersInLobby().size());
 		Thread.sleep(LobbyController.TIMEOUT_MILLISECONDS / 2);
 		// After a while there should still be two. Bob refreshes his timer.
-		assertEquals(2, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby());
+		assertEquals(2, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby().size());
 		Thread.sleep(LobbyController.TIMEOUT_MILLISECONDS / 2 + 10);
 		// Now, Alice should have timed out and bob should see only one player in the lobby.
-		assertEquals(1, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby());
+		assertEquals(1, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby().size());
 		// A different Alice joins now and Bob should see two players again.
 		assertEquals(HttpStatus.ACCEPTED, lobby.registerPlayer("Alice").getStatusCode());
-		assertEquals(2, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby());
+		assertEquals(2, lobby.refreshPlayer("Bob", bob.playerID()).getBody().playersInLobby().size());
+	}
+
+	@Test
+	public void startGame() throws IOException {
+		LobbyController lobby = new LobbyController();
+
+		// Add two players to lobby.
+		LobbyResponse alice = lobby.registerPlayer("Alice").getBody();
+		LobbyResponse bob = lobby.registerPlayer("Bob").getBody();
+
+		// Start the game on the server.
+		LobbyResponse response = lobby.startGame("Alice", alice.playerID()).getBody();
+
+		// Make sure the game started and the port is valid.
+		assertTrue(response.gameStarted());
+		assertNotEquals(-1, response.tcpPort());
+		assertNotEquals(0, response.tcpPort());
+
+		// Bob also sees that the game started.
+		LobbyResponse refreshResponse = lobby.refreshPlayer("Bob", bob.playerID()).getBody();
+		assertTrue(refreshResponse.gameStarted());
+		assertEquals(response.tcpPort(), refreshResponse.tcpPort());
+
+		// Try making a connections from both players.
+		Connection aliceConn = Connection.to("localhost", response.tcpPort());
+		aliceConn.send(new JoinMessage("Alice"));
+		Connection bobConn = Connection.to("localhost", response.tcpPort());
+		bobConn.send(new JoinMessage("Bob"));
 	}
 }
