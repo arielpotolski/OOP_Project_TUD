ce727b561d4e2e157421b6b5114c2c7f20ea2a63
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2022-03-21 17:34:40 +0100

    Add waiting screen (lobby)

--- client/src/main/java/client/scenes/WaitingScreenCtrl.java
+++ client/src/main/java/client/scenes/WaitingScreenCtrl.java
@@ -1,11 +1,20 @@
 package client.scenes;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import client.utils.ServerUtils;
+import commons.LobbyResponse;
+
+import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class WaitingScreenCtrl {
+	private MainCtrl mainCtrl;
+
 	@FXML
 	private AnchorPane peopleInTheLeaderBoardPane;
 
@@ -14,4 +23,9 @@ public class WaitingScreenCtrl {
 
 	@FXML
 	private Label peopleInTheRoomLabel;
+
+	@Inject
+	public WaitingScreenCtrl(MainCtrl mainCtrl) {
+		this.mainCtrl = mainCtrl;
+	}
 }
