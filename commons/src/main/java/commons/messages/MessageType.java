ad7e78cf385a3ddb0c41ac961744df1381f273f7
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-03-28 16:21:50 +0200

    Add MessageType for Leaderboard

--- commons/src/main/java/commons/messages/MessageType.java
+++ commons/src/main/java/commons/messages/MessageType.java
@@ -6,4 +6,4 @@ package commons.messages;
  * The type of the message. Each subclass of Message should have its own value in this enum.
  * They should then return it in their `getType()` method. (See `Message`).
  */
-public enum MessageType { ERROR, JOIN }
+public enum MessageType { ERROR, JOIN, LEADERBOARD }
