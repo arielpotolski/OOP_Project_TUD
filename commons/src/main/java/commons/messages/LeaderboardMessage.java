17162597c0bae25ac9990aa9b7a1589092fd65f7
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-03-28 16:25:10 +0200

    Add the LeaderBoard message as a class

---
+++ commons/src/main/java/commons/messages/LeaderboardMessage.java
@@ -0,0 +1,25 @@
+package commons.messages;
+
+import java.util.HashMap;
+
+
+/**
+ * Sent by the client after they create the initial TCP connection
+ * so that the server has a name to identify the connection with.
+ */
+public class LeaderboardMessage extends Message {
+	@Override
+	public MessageType getType() {
+		return MessageType.LEADERBOARD;
+	}
+
+	private final HashMap<String, Integer> players;
+
+	public LeaderboardMessage(HashMap<String, Integer> players) {
+		this.players = players;
+	}
+
+	public HashMap<String, Integer> getPlayers() {
+		return this.players;
+	}
+}
