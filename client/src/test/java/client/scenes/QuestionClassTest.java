3d4ea7c780b0005fca35c3ade1905b97ae721332
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2022-03-31 10:15:47 +0200

    Fix pipeline

--- client/src/test/java/client/scenes/QuestionClassTest.java
+++ client/src/test/java/client/scenes/QuestionClassTest.java
@@ -1,7 +1,6 @@
 package client.scenes;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
