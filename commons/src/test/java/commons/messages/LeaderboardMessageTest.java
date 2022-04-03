344f1280e108bd174a1836e11ff836a448dccf5f
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-04-03 18:23:53 +0200

    Test leaderboard message class

---
+++ commons/src/test/java/commons/messages/LeaderboardMessageTest.java
@@ -0,0 +1,37 @@
+package commons.messages;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+
+public class LeaderboardMessageTest {
+
+	private LeaderboardMessage message;
+
+	@BeforeEach
+	void setUp() {
+		HashMap<String, Integer> leaderboard = new HashMap<>();
+		leaderboard.put("Dimitar", 150);
+		leaderboard.put("Ganio", 560);
+		this.message = new LeaderboardMessage(leaderboard);
+	}
+
+	@Test
+	void getType() {
+		assertEquals(MessageType.LEADERBOARD, this.message.getType());
+	}
+
+	@Test
+	void getPlayers() {
+		HashMap<String, Integer> map = this.message.getPlayers();
+		assertTrue(map.containsKey("Dimitar"));
+		assertTrue(map.containsKey("Ganio"));
+		assertEquals(150, map.get("Dimitar"));
+		assertEquals(560, map.get("Ganio"));
+	}
+}
