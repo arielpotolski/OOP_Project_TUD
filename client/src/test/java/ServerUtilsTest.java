4cfe0818f025d952be32be05f682d241e01da525
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-04-03 16:07:40 +0200

    Test getQuestions, getPlayers, getActivities

---
+++ client/src/test/java/ServerUtilsTest.java
@@ -0,0 +1,214 @@
+import java.io.IOException;
+
+import client.utils.ServerUtils;
+import commons.Activity;
+import commons.HighestConsumptionQuestion;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ServerUtilsTest {
+
+	private WireMockServer mockServer;
+	private ServerUtils serverUtils;
+	ResponseDefinitionBuilder response;
+
+
+	@BeforeEach
+	void setUp() {
+		this.response = new ResponseDefinitionBuilder();
+		this.response.withHeader("Content-type", "application/json");
+		this.response.withStatus(200);
+		this.mockServer = new WireMockServer();
+		this.mockServer.start();
+		WireMock.configureFor("localhost", 8080);
+		this.serverUtils = new ServerUtils("localhost");
+	}
+
+	@AfterEach
+	void tearDown() {
+		this.mockServer.stop();
+	}
+
+	@Test
+	void getConnection() {
+	}
+
+	@Test
+	void getServer() {
+
+	}
+
+	@Test
+	void addPlayer() {
+
+	}
+
+	@Test
+	void getQuestions() {
+		this.response.withBody("[{" +
+				"        \"type\": \"HighestConsumptionQuestion\",\n" +
+				"        \"activity1\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"66-router\",\n" +
+				"            \"title\": \"A WIFI router for one day\",\n" +
+				"            \"consumption_in_wh\": 144,\n" +
+				"            \"image_path\": \"66/router.jpg\",\n" +
+				"            \"source\": \"https://www.makeitcheaper.com.au/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"activity2\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"34-netflix\",\n" +
+				"            \"title\": \"Watching Netflix for 1 hour\",\n" +
+				"            \"consumption_in_wh\": 6100,\n" +
+				"            \"image_path\": \"34/netflix.png\",\n" +
+				"            \"source\": \"https://www.iea.org/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"activity3\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"78-airconditioner\",\n" +
+				"            \"title\": \"Using an air conditioner for 30 minutes\",\n" +
+				"            \"consumption_in_wh\": 1500,\n" +
+				"            \"image_path\": \"78/airconditioner.jpg\",\n" +
+				"            \"source\": \"https://americanhomewater.com/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"choice1\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"66-router\",\n" +
+				"            \"title\": \"A WIFI router for one day\",\n" +
+				"            \"consumption_in_wh\": 144,\n" +
+				"            \"image_path\": \"66/router.jpg\",\n" +
+				"            \"source\": \"https://www.makeitcheaper.com.au/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"choice2\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"34-netflix\",\n" +
+				"            \"title\": \"Watching Netflix for 1 hour\",\n" +
+				"            \"consumption_in_wh\": 6100,\n" +
+				"            \"image_path\": \"34/netflix.png\",\n" +
+				"            \"source\": \"https://www.iea.org/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"choice3\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"78-airconditioner\",\n" +
+				"            \"title\": \"Using an air conditioner for 30 minutes\",\n" +
+				"            \"consumption_in_wh\": 1500,\n" +
+				"            \"image_path\": \"78/airconditioner.jpg\",\n" +
+				"            \"source\": \"https://americanhomewater.com/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"title_3\": \"Using an air conditioner for 30 minutes\",\n" +
+				"        \"correctAnswer\": {\n" +
+				"            \"valid\": true,\n" +
+				"            \"id\": \"34-netflix\",\n" +
+				"            \"title\": \"Watching Netflix for 1 hour\",\n" +
+				"            \"consumption_in_wh\": 6100,\n" +
+				"            \"image_path\": \"34/netflix.png\",\n" +
+				"            \"source\": \"https://www.iea.org/\",\n" +
+				"            \"array-image\": \"\"\n" +
+				"        },\n" +
+				"        \"title_1\": \"A WIFI router for one day\",\n" +
+				"        \"title_2\": \"Watching Netflix for 1 hour\",\n" +
+				"        \"image_path_3\": \"78/airconditioner.jpg\",\n" +
+				"        \"image_path_1\": \"66/router.jpg\",\n" +
+				"        \"image_path_2\": \"34/netflix.png\"\n" +
+				"    }\n" +
+				"]");
+		this.mockServer.stubFor(get("/api/questions/?seed=1").willReturn(this.response));
+		HighestConsumptionQuestion result = (HighestConsumptionQuestion)
+				(this.serverUtils.getQuestions(1).get(0));
+		assertEquals(144, result.getActivity1().getConsumptionInWh());
+		assertEquals(6100, result.getActivity2().getConsumptionInWh());
+	}
+
+	@Test
+	void getActivities() throws IOException {
+		Activity activity1 = new Activity("123", "title", 230, "pathpng", "some site");
+		this.response.withBody("[{\"valid\": true,\n" +
+				"        \"id\": \"123\",\n" +
+				"        \"title\": \"title\",\n" +
+				"        \"consumption_in_wh\": 230,\n" +
+				"        \"image_path\": \"pathpng\",\n" +
+				"        \"source\": \"some site\",\n" +
+				"        \"array-image\": \"/9j/4AAQSkZJRgABAgAAAQABAAD/\"}]");
+		this.mockServer.stubFor(get("/api/questions/getActivities").willReturn(this.response));
+		assertEquals(activity1.getTitle(), this.serverUtils.getActivities().get(0).getTitle());
+		assertEquals(activity1.getConsumptionInWh(),
+				this.serverUtils.getActivities().get(0).getConsumptionInWh());
+	}
+
+	@Test
+	void getPlayers() {
+		this.response.withBody("[{\n" +
+				"        \"id\": 1,\n" +
+				"        \"nickname\": \"Dimitar\",\n" +
+				"        \"points\": 1484\n" +
+				"    }]");
+		this.mockServer.stubFor(get("/api/players/").willReturn(this.response));
+		assertEquals("Dimitar",
+				this.serverUtils.getPlayers().get(0).getNickName());
+	}
+
+	@Test
+	void connectToLobby() {
+
+	}
+
+	@Test
+	void refreshLobby() {
+
+	}
+
+	@Test
+	void startMultiplayerGame() {
+
+	}
+
+	@Test
+	void makeConnection() throws IOException {
+//		this.serverUtils.makeConnection(8843);
+
+	}
+
+	@Test
+	void addActivity() {
+
+	}
+
+	@Test
+	void removeActivity() {
+
+	}
+
+	@Test
+	void connect() {
+
+	}
+
+	@Test
+	void registerForMessages() {
+
+	}
+
+	@Test
+	void send() {
+
+	}
+
+	@Test
+	void setSession() {
+
+	}
+}
