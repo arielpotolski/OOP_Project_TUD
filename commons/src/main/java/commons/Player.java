b5ce42fd40211a8d8c5ebcf791f1eceb0ac34d51
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-04-03 18:23:18 +0200

    Fi toString() method because it took into consideration the memory place of the object

--- commons/src/main/java/commons/Player.java
+++ commons/src/main/java/commons/Player.java
@@ -7,8 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 @Entity
 public class Player {
@@ -62,6 +60,10 @@ public class Player {
 
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+		return "Player{" +
+				"id=" + this.id +
+				", nickname='" + this.nickname + '\'' +
+				", points=" + this.points +
+				'}';
 	}
 }
