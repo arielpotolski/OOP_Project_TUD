8c6d447c342d20c8623e2e3620d6095f237459c6
Dimitar Nikolov Nikolov <D.N.Nikolov-1@student.tudelft.nl>
2022-04-03 18:23:29 +0200

    Test player class

---
+++ commons/src/test/java/commons/PlayerTest.java
@@ -0,0 +1,58 @@
+package commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PlayerTest {
+
+	private Player player;
+
+	@BeforeEach
+	void setUp() {
+		this.player = new Player("Dimitar", 989);
+	}
+
+	@Test
+	void constructorTest() {
+		Player examplePlayer = new Player("Don");
+		assertNotNull(examplePlayer);
+		assertNotNull(this.player);
+	}
+
+	@Test
+	void getPoint() {
+		assertEquals(989, this.player.getPoint());
+	}
+
+	@Test
+	void setPoint() {
+		this.player.setPoint(976);
+		assertEquals(976, this.player.getPoint());
+	}
+
+	@Test
+	void getNickname() {
+		assertEquals("Dimitar", this.player.getNickName());
+	}
+
+	@Test
+	void setNickname() {
+		this.player.setNickname("Doko");
+		assertEquals("Doko", this.player.getNickName());
+	}
+
+	@Test
+	void equalsAndHash() {
+		Player equalPl = new Player("Dimitar", 989);
+		assertEquals(equalPl, this.player);
+		assertEquals(equalPl.hashCode(), this.player.hashCode());
+	}
+
+	@Test
+	void toStringTest() {
+		String result = "Player{id=0, nickname='Dimitar', points=989}";
+		assertEquals(result, this.player.toString());
+	}
+}
