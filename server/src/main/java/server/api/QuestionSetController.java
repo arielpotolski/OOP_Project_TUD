4ed4780e2e77e1600fd255fb121bf426b8d7f314
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2022-04-05 00:16:53 +0200

    Fix bug caught by tests

--- server/src/main/java/server/api/QuestionSetController.java
+++ server/src/main/java/server/api/QuestionSetController.java
@@ -41,7 +41,7 @@ public class QuestionSetController {
 	public List<Question> getAll(@RequestParam long seed) {
 		List<Activity> as = this.repository.findAll();
 		QuestionSet qs = new QuestionSet(as, seed);
-		qs.fillSet(20);
+		qs.fillSet(Math.min(as.size(), 20));
 		return qs.getQuestions();
 	}
 
